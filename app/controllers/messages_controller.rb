cecf13edd5d79fba13d06563d36941f2c9dcc57f
Ysatuma <yssatuma@gmail.com>
2020-05-26 14:08:37 +0900

    install carrierwace, make submit function
    carrierwaveの導入と投稿送信機能の実装を行った。

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,30 @@
 class MessagesController < ApplicationController
+  before_action :set_group    
+
   def index
-    
-    @groups = Group.includes(:users).where(users: {id: current_user})
+    @groups = Group.includes(:users).where(users: {id: current_user}) 
+    @messages = @group.messages
+    @message = Message.new
+  end
+  
+  def create
+    @message = @group.messages.new(message_params)
+    if @message.save
+      redirect_to group_messages_path, notice: 'メッセージを保存しました'
+    else
+      render :index, notice: 'メッセージを入力してください'
+    end
+  end
+
+
+  private
+  
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
+  def set_group
     @group = Group.find(params[:group_id])
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   root 'groups#index'
 
   resources :users, only: [:edit, :update, :delete]
-  resources :groups, only: [:index, :new, :create, :edit, :update] do
-    resources :messages, only: :index
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
   end
  
 
